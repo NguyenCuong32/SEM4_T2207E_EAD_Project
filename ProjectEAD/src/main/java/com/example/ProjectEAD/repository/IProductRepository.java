a9bc15bbea13a036b3cbc2ec57fc90038b0e54c5
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2024-03-24 21:34:50 +0700

    1
    1

--- ProjectEAD/src/main/java/com/example/ProjectEAD/repository/IProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ProjectEAD.repository;
-
-import com.example.ProjectEAD.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProductRepository extends JpaRepository<Product, Integer> {
-}
