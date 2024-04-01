dfefe09d750050b5b600361f7daef6dadf6947d5
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2024-04-01 20:07:28 +0700

    up store pocedure
    up store pocedure

--- PROCEDURE.sql
+++ PROCEDURE.sql
@@ -1,6 +1,6 @@
-CREATE PROCEDURE GetUserByUsername(IN username VARCHAR(255))
+CREATE PROCEDURE GetUserByUsername(IN usernametk VARCHAR(255))
 BEGIN
-    SELECT username, password, status FROM users WHERE username = username;
+    SELECT username, password, status FROM users WHERE username = usernametk;
 END;
 
 CREATE PROCEDURE GetAuthoritiesByUsername(IN username VARCHAR(255))
@@ -11,3 +11,21 @@ BEGIN
     INNER JOIN roles r ON ur.role_id = r.id 
     WHERE u.username = username;
 END;
+
+--Phân trang
+CREATE PROCEDURE GetPaginatedData(
+    IN pageNumber INT,
+    IN pageSize INT,
+    IN tableName VARCHAR(255)
+)
+BEGIN
+    DECLARE offsetValue INT DEFAULT 0;
+    SET offsetValue = (pageNumber - 1) * pageSize;
+
+    SET @query = CONCAT('SELECT * FROM ', tableName, ' LIMIT ', offsetValue, ',', pageSize);
+
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END;
+
